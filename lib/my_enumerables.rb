0ec3b4ff3847add812c2b9e796f2333abf41e790
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-12-09 19:58:35 -0600

    Add my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    count = 0
+    my_each do |element|
+      yield element, count
+      count += 1
+    end
+  end
 end
 
 # You will first have to define my_each
